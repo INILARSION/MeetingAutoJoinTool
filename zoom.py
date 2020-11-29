8115dcde47627738937704e5bf0cb293e190de5c
timlueg <7452348+timlueg@users.noreply.github.com>
2020-11-30 00:04:54 +0100

    zoom retry join if meeting window did not open

--- zoom.py
+++ zoom.py
@@ -16,11 +16,21 @@ class ZoomAutomation(MeetingAutomation):
         self.zoom_process = None
 
     def start_meeting(self):
+        self._start_zoom_process(self.url)
+        time.sleep(25)
+
+        while not self._is_meeting_window_open():
+            os.kill(self.zoom_process.pid, signal.SIGKILL)
+            time.sleep(5)
+            self._start_zoom_process(self.url)
+            time.sleep(60)
+
+    def _start_zoom_process(self, url: str):
         if shutil.which('zoom'):
-            arg = '--url=zoommtg://' + self.url
+            arg = '--url=zoommtg://' + url
             cmd = ["zoom", arg]
         elif shutil.which('zoom-client'):
-            arg = '--url=' + self.url
+            arg = '--url=' + url
             cmd = ["zoom-client", arg]
             print(cmd)
         else:
@@ -28,7 +38,14 @@ class ZoomAutomation(MeetingAutomation):
             print("Run 'snap install zoom' or 'sudo apt install zoom'")
             raise Exception
         self.zoom_process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        time.sleep(25)
+
+    @staticmethod
+    def _is_meeting_window_open() -> bool:
+        """ Zoom opens window "zoom - free account" (on ubuntu, gnome-shell) """
+        list_windows = display.Display().screen().root.query_tree()
+        window_names = [x.get_wm_name().lower() for x in list_windows.children]
+
+        return 'zoom -' in window_names
 
     def end_meeting(self, is_hotkeys_used):
         super().stop_recording(is_hotkeys_used)
